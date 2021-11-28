56a5dbe9490f48119113e0c827f1f7d51fa4c9bb
StanleyPangaruy <pangaruystanley@gmail.com>
2021-11-28 13:11:49 +0800

    defined functions for display and main that runs the whole program and added a feature in user's sentence input that follows the rule of constructing a sentence

--- program1.py
+++ program1.py
@@ -10,42 +10,59 @@
 
 
 def userInput():
-    return input('Sentence: ')
+    while True:
+        sentence = input('Enter your sentence: ')
+        capital = sentence[0]
+        punctuation = sentence[-1]
+        if not capital.isupper():
+            print('Please start with a capital letter.')
+            continue
+        elif punctuation not in ['.', '?', '!']:
+            print('Please end with a punctuation mark.')
+            continue
+        else:
+            return sentence
 
 def wordCount(sentence):
     word = (' ')
     wordCount = 1
-    for c in sentence:
-        if c in word:
+    for count in sentence:
+        if count in word:
             wordCount +=  1
     return wordCount
 
 def vowelCount(sentence):
     vowels = ('AaEeIiOoUu')
     vowelCount = 0
-    for c in sentence:
-        if c in vowels:
+    for count in sentence:
+        if count in vowels:
             vowelCount +=  1
     return vowelCount
 
 def consonantCount(sentence):
     consonants = ('BbCcDdFfGgHhJjKkLlMmNnPpQqRrSsTtVvWwXxYyZz')
     consonantCount = 0
-    for c in sentence:
-        if c in consonants:
+    for count in sentence:
+        if count in consonants:
             consonantCount +=  1
     return consonantCount
 
+def display(wordC,vowelC,consonantC):
+    print('Word count:', wordC)
+    print('Vowel letter count:', vowelC)
+    print('Consonant letter count:', consonantC)
 
-    
+def main():
+    sentence = userInput()
+    wordCount(sentence)
+    vowelCount(sentence)
+    consonantCount(sentence)
+    wCount = wordCount(sentence)
+    vCount = vowelCount(sentence)
+    cCount = consonantCount(sentence)
+    display(wCount,vCount,cCount)
 
-
-
-
-
-
-
-
+main()
 
 
 
